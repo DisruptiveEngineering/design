2101db492836ebd5a8e14b878a3efd1724a29e2d
Troy Bergland <troybergland@gmail.com>
2025-09-04 14:36:26 +0200

    Initial commit
    Initial setup of Designsystemet.no for use with Tokens Studio

---
+++ design-tokens-build/colors.d.ts
@@ -0,0 +1,21 @@
+/* This file is deprecated and will be removed in a future release. Use types.d.ts instead */
+/* build: v1.4.0 */
+import type {} from '@digdir/designsystemet/types';
+
+// Augment types based on theme
+declare module '@digdir/designsystemet/types' {
+  export interface ColorDefinitions {
+    accent: never;
+    'veira-coral': never;
+    'veria-yellow': never;
+    'veira-green': never;
+    'veira-blue': never;
+    neutral: never;
+  }
+  export interface SeverityColorDefinitions {
+    info: never;
+    success: never;
+    warning: never;
+    danger: never;
+  }
+}
